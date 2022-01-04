0b912e8666b3d7886dafe28fba10b9549ca3ff21
Ben North <ben@redfrontdoor.org>
2022-01-04 15:27:36 +0000

    image_from_filename(): Add

--- pico/make_phis.py
+++ pico/make_phis.py
@@ -1,3 +1,16 @@
 """
 Script to generate CPP code initialising phi and Dphi arrays
 """
+
+from functools import reduce
+from operator import concat
+
+
+def image_from_filename(filename):
+    with open(filename, "rt") as f_in:
+        rows = [[int(ch) for ch in line.strip()] for line in f_in]
+        if len(rows) != 8:
+            raise ValueError(f"expecting 8 rows but got {len(rows)}")
+        if any(len(row) != 8 for row in rows):
+            raise ValueError("expecting 8 columns in every row")
+        return reduce(concat, rows, [])
