095d4fcf07d7a49cc264373059b9d6bdabbcd0e4
Ben North <ben@redfrontdoor.org>
2022-01-04 16:27:12 +0000

    Define pin-number and related constants

--- pico/flashing-lights.cpp
+++ pico/flashing-lights.cpp
@@ -2,6 +2,12 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
+#include <cstdint>
+
+static const uint8_t PIN_MOSI = 19;
+static const uint8_t PIN_SCK = 18;
+static const uint8_t PIN_CS = 17;
+static const uint8_t CHAIN_LEN = 4;
 
 int main()
 {
