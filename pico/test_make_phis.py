f736b3b88d746f956060cb1616785e5235fe655b
Ben North <ben@redfrontdoor.org>
2022-01-04 16:06:52 +0000

    cpp_array(): Test

--- pico/test_make_phis.py
+++ pico/test_make_phis.py
@@ -15,3 +15,47 @@ class TestPhiIncrChoices:
 
     def test_odd_freq0_rejected(self):
         pytest.raises(ValueError, make_phis.PhiIncrChoices.make, 11, 2)
+
+
+class TestCppArray:
+    def test_complete_last_row(self):
+        got_txt = make_phis.cpp_array(
+            "foo",
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+             201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212]
+        )
+        exp_txt = "\n".join([
+            "uint32_t foo[24] = {",
+            "  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,",
+            "  201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212",
+            "};\n"
+        ])
+        assert got_txt == exp_txt
+
+    def test_incomplete_last_row(self):
+        got_txt = make_phis.cpp_array(
+            "foo",
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+             201, 202, 203, 204, 205, 206, 207, 208]
+        )
+        exp_txt = "\n".join([
+            "uint32_t foo[20] = {",
+            "  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,",
+            "  201, 202, 203, 204, 205, 206, 207, 208",
+            "};\n"
+        ])
+        assert got_txt == exp_txt
+
+    def test_singleton_last_row(self):
+        got_txt = make_phis.cpp_array(
+            "foo",
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+             201]
+        )
+        exp_txt = "\n".join([
+            "uint32_t foo[13] = {",
+            "  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,",
+            "  201",
+            "};\n"
+        ])
+        assert got_txt == exp_txt
