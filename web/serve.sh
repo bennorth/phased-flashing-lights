baf2e2a3cb2c49288826c600715d84272134f09e
Ben North <ben@redfrontdoor.org>
2022-01-04 09:37:45 +0000

    Add script to serve with live-reload

---
+++ web/serve.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# TERM everything in process group when script exits.
+# https://stackoverflow.com/a/2173421
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
+[ -x ./node_modules/.bin/livereloadx ] || {
+    echo Could not find livereloadx
+    exit 1
+}
+
+(
+    cd dist
+    exec ../node_modules/.bin/livereloadx --delay 100
+) &
+
+(
+    cd dist
+    exec python3 ../serve.py
+) &
+
+ls -1 index.md phased-flashing.css phased-flashing.js | exec entr ./make.sh dev
