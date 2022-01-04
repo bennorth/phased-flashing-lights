9e6c9426b63c10887aa2059c06eaedca7a45f321
Ben North <ben@redfrontdoor.org>
2022-01-04 17:30:42 +0000

    Add copyright/licence notices

--- pico/make_phis.py
+++ pico/make_phis.py
@@ -1,5 +1,23 @@
 """
 Script to generate CPP code initialising phi and Dphi arrays
+
+Copyright 2022 Ben North
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+No copyright or other claims are made over the output C++ text
+produced by this program.
 """
 
 from functools import reduce
