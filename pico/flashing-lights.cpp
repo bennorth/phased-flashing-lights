3a8f58399d7e1f082f7e55aa8f03d94920789089
Ben North <ben@redfrontdoor.org>
2022-01-04 16:27:29 +0000

    Declare pico7219-related constant, type, pointer

--- pico/flashing-lights.cpp
+++ pico/flashing-lights.cpp
@@ -3,11 +3,17 @@
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 #include <cstdint>
+#include "pico7219.h"
 
 static const uint8_t PIN_MOSI = 19;
 static const uint8_t PIN_SCK = 18;
 static const uint8_t PIN_CS = 17;
 static const uint8_t CHAIN_LEN = 4;
+static const PicoSpiNum SPI_CHAN = PICO_SPI_0;
+
+typedef struct Pico7219 Pico7219;
+
+static Pico7219 * pico7219;
 
 int main()
 {
