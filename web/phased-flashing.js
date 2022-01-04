b5a0949484a2b2c1c85c049dff707956bc5e8863
Ben North <ben@redfrontdoor.org>
2022-01-04 08:55:31 +0000

    LEDMatrix: Add constants and ctor

--- web/phased-flashing.js
+++ web/phased-flashing.js
@@ -1,2 +1,25 @@
+const LED_Side_Proportion = 0.9;
+const Matrix_Border_Proportion = 1.0 - LED_Side_Proportion;
+
 class LEDMatrix {
+    constructor(canvas, cellSize, nRows, nCols) {
+        this.canvas = canvas;
+        this.cellSize = cellSize;
+        this.nCols = nCols;
+        this.ledSide = LED_Side_Proportion * cellSize;
+
+        const border = Matrix_Border_Proportion * cellSize;
+
+        this.width = canvas.width = cellSize * nCols + border;
+        this.height = canvas.height = cellSize * nRows + border;
+
+        $(canvas).css({
+            width: canvas.width,
+            height: canvas.height,
+            backgroundColor: "black",
+        });
+
+        this.context = canvas.getContext("2d");
+        this.context.translate(border, border);
+    }
 }
