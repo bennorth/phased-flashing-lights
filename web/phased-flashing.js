1131366eda5e0ac86507df6f1ef5d2d4016cb2b3
Ben North <ben@redfrontdoor.org>
2022-01-04 17:45:50 +0000

    Add copyright notice

--- web/phased-flashing.js
+++ web/phased-flashing.js
@@ -1,3 +1,21 @@
+// Simulate display of images in flashing lights
+//
+// Copyright 2022 Ben North
+//
+// This program is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 const Two_Pi = 2.0 * Math.PI;
 const LED_Side_Proportion = 0.9;
 const Matrix_Border_Proportion = 1.0 - LED_Side_Proportion;
